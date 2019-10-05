eb484da98828de7d2eb49038500492b06ea4f729
Brian O'Connor <bocon@opennetworking.org>
2019-10-04 17:26:21 -0700

    Fixes for Docker containers on Linux
    Fixing host.docker.internal
    - For p4rt-shell and gnmi-cli, use host networking
    - For onos, use container that has patch /etc/hosts

--- basic/start-onos.sh
+++ basic/start-onos.sh
@@ -3,4 +3,4 @@
 docker run -it --rm \
   -e ONOS_APPS=gui2,drivers.bmv2,pipelines.basic,lldpprovider,hostprovider,fwd \
   -p 8101:8101 -p 8181:8181 \
-  onosproject/onos:2.1.0
+  onosproject/onos:2.2.x
